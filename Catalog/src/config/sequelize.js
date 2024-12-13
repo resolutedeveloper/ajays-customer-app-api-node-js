2cd4d671b9fcbc85c764dc87d5cd8b57e9803799
Patel Rinku <rinku@resolutesolutions.in>
2024-12-13 19:02:51 +0530

    deploy testing

--- Catalog/src/config/sequelize.js
+++ Catalog/src/config/sequelize.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DB_NAME_CATALOG,
     process.env.DB_USER,
-    process.env.DB_PASSWORD_S,
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
