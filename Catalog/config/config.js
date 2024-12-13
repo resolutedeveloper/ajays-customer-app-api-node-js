dfb86b76189a673154c8cc2cabc909cde7e85714
Patel Rinku <rinku@resolutesolutions.in>
2024-12-13 18:46:30 +0530

    deploy testing

--- Catalog/config/config.js
+++ Catalog/config/config.js
@@ -1,21 +1,21 @@
 module.exports = {
     development: {
         username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
+        password: process.env.DB_PASSWORD_S || '',
         database: process.env.DB_NAME || 'CatalogDB',
         host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
     },
     test: {
         username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
+        password: process.env.DB_PASSWORD_S || '',
         database: process.env.DB_NAME || 'CatalogDB',
         host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
     },
     production: {
         username: process.env.DB_USER || 'root',
-        password: process.env.DB_PASSWORD || '',
+        password: process.env.DB_PASSWORD_S || '',
         database: process.env.DB_NAME || 'CatalogDB',
         host: process.env.DB_HOST || 'localhost',
         dialect: 'mysql',
--- Catalog/src/config/sequelize.js
+++ Catalog/src/config/sequelize.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PASSWORD_S,
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
