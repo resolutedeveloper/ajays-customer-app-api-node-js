ef814f4b2ac86045e28603bb7b4a1d2e329964aa
Patel Rinku <rinku@resolutesolutions.in>
2024-12-14 10:58:50 +0530

    code setup

--- Customer/src/config/sequelize.js
+++ Customer/src/config/sequelize.js
@@ -1,13 +1,13 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
+    process.env.DB_NAME_CUSTOMER,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         dialect: 'mysql',
-        timezone: '+00:00', // Set your timezone here (e.g., '+00:00' for UTC)
+        timezone: '+05:30', // Set your timezone here (e.g., '+00:00' for UTC)
         logging: false, // Disable SQL query logging (optional)
     }
 );
