92552fd7c1185d6f89cccf6817e9a035916674bd
Harsh Patel <rsdeveloper.intern6@yahoo.com>
2024-12-23 16:20:12 +0530

    company model

---
+++ Catalog/src/models/companyModel.js
@@ -0,0 +1,49 @@
+module.exports = (sequelize, DataTypes)=>{
+    const companyModel = sequelize.define("Company",{
+        CompanyID:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        CompanyName:{
+            type: DataTypes.STRING(300),
+        },
+    CompanySName:{
+        type: DataTypes.STRING(500),
+    },
+    CompanyLogo:{
+        type: DataTypes.STRING(500),
+    },
+    ContactNo1:{
+        type: DataTypes.STRING(300),
+    },
+    ContactNo2:{
+        type: DataTypes.STRING(300),
+    },
+    Address:{
+        type: DataTypes.STRING(500),
+    },
+    AddressLine2:{
+        type: DataTypes.STRING(500),
+    },
+    AddressLine3:{
+        type: DataTypes.STRING(500),
+    },
+    GSTNo:{
+        type: DataTypes.STRING(500),
+    },
+    GSTWEF:{
+        type: DataTypes.DATE,
+    },
+    IsVisible:{
+        type: DataTypes.BOOLEAN,
+    },
+    Remarks:{
+        type: DataTypes.STRING(500),
+    },
+    MarchantID:{
+        type: DataTypes.STRING(500),
+    },
+    },{timeStamps: false});
+
+    return companyModel;
+}
