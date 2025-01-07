6035895241b2d906583ba08ac48647b9b6b2c266
Harsh Patel <rsdeveloper.intern6@yahoo.com>
2025-01-07 18:39:21 +0530

    new change

--- Customer/src/routes/index.js
+++ Customer/src/routes/index.js
@@ -5,7 +5,6 @@ const CustomerToken = require("../middelware/CustomerTokenVerification.js");
 route.use("/auth", require("./CustomerAuthRoutes.js"));
 route.use("/customer",CustomerToken, require("./CustomerDashBoardRoutes.js"));
 route.use("/favoriteLocation",CustomerToken, require("./favoriteLocationRoutes.js"));
-route.use("/customers", require("./customerRoutes.js"));
 route.use("/customefcm",CustomerToken, require("./CustomerFcmRoutes.js"))
 route.use("/customeVersion",CustomerToken, require("./CustomerValidationRouter.js"))
 route.use("/customerHistory",CustomerToken, require("./historyCustomerRoutes.js"))
