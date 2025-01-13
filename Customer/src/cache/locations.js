2dd5baae4019e6cb272c7fb7123ad4108ff66219
Patel Rinku <rinku@resolutesolutions.in>
2025-01-13 15:39:54 +0530

    git radis update

---
+++ Customer/src/cache/locations.js
@@ -0,0 +1,29 @@
+const redis = require("redis");
+const COLOR = "\x1b[31m";
+const RESET = "\x1b[0m";
+
+const internalUrl = process.env.REDIS_URL_INTERNAL;
+const portRedis = process.env.REDIS_PORT;
+
+const client = redis.createClient({
+    url: `redis://${internalUrl}:${portRedis}`
+});
+
+client.on("error", (err) => {
+    console.error("Redis Client Error", err);
+});
+
+const redisConnection = async () => {
+    try {
+        await client.connect();
+        await client.set("AdminPing", "Redis Admin Pong", {
+            NX: true,
+        });
+        const test = await client.get("ClientPing");
+        console.log(`${COLOR}${test}${RESET}`);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+module.exports = { client, redisConnection };
