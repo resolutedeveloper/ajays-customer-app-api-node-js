8d8f9a3c825bde568ae98f1b9f29fc988e966237
Harsh Patel <rsdeveloper.intern6@yahoo.com>
2025-01-20 10:58:50 +0530

    change

--- Catalog/src/routes/itemRoutes.js
+++ Catalog/src/routes/itemRoutes.js
@@ -1,10 +1,6 @@
 const express = require('express');
-<<<<<<< HEAD
-const { getItemDetails, getCatalogData, saveImage } = require('../controllers/itemController');
-=======
 const { getItemDetails, getCatalogData } = require('../controllers/itemController');
 
->>>>>>> cf59e4e18c3e5d73111f7a2a7b8197f41b177659
 const validateCatalogToken = require('../middleware/TokenVerification');
 const router = express.Router();
 
