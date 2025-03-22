9e42aa74f6d019e475ae15d3d077707e45ec13dd
AshwinTiwary <ashwin@resolutesolutions.in>
2025-03-22 11:43:22 +0530

    Changes

--- Customer/src/controllers/CustomerPayment.js
+++ Customer/src/controllers/CustomerPayment.js
@@ -2,14 +2,21 @@ const { encrypt, decrypt } = require("../helpers/ccavutil");
 
 async function startPayment(req, res) {
     try {
+        const { amount, orderId } = req.body;
+        if (!amount || !orderId) {
+            return res.status(400).json({
+                message: "Amount/OrderId not present."
+            })
+        }
+
         let merchantId = process.env.MERCHANT_ID;
         let accessCode = process.env.ACCESS_CODE;
         let workingKey = process.env.WORKING_KEY.trim();
         let redirectUrl = process.env.REDIRECT_URL;
         let cancelUrl = process.env.CANCEL_URL;
 
-        let orderId = `ORDER_${Date.now()}`;
-        let amount = req.body.amount || "1.00";
+        // let orderId = `ORDER_${Date.now()}`;
+        // let amount = req.body.amount || "1.00";
 
         let postData = `merchant_id=${merchantId}&order_id=${orderId}&amount=${amount}&currency=INR&redirect_url=${redirectUrl}&cancel_url=${cancelUrl}`;
 
@@ -17,15 +24,15 @@ async function startPayment(req, res) {
 
         let encRequest = encrypt(postData, workingKey);
 
-    //     let toSend = `
-    //     <form method="post" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
-    //         <input type="hidden" name="encRequest" value="${encRequest}">
-    //         <input type="hidden" name="access_code" value="${accessCode}">
-    //         <button type="submit">Pay Now</button>
-    //     </form>
-    // `;
+        //     let toSend = `
+        //     <form method="post" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction">
+        //         <input type="hidden" name="encRequest" value="${encRequest}">
+        //         <input type="hidden" name="access_code" value="${accessCode}">
+        //         <button type="submit">Pay Now</button>
+        //     </form>
+        // `;
 
-    let toSend = `https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&encRequest=${encRequest}&access_code=${accessCode}`
+        let toSend = `https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction&encRequest=${encRequest}&access_code=${accessCode}`
 
         return res.send(toSend);
     } catch (error) {
